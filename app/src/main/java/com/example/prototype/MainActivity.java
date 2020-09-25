5e0604cd0e663548790e78f27e183b126bb3928b
Rajprasad <rajprasaddora2000@gmail.com>
2020-09-26 02:52:52 +0530

    all layouts prepareed

--- app/src/main/java/com/example/prototype/MainActivity.java
+++ app/src/main/java/com/example/prototype/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.prototype;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View bottomSheet =findViewById(R.id.shape_of_bottomSheet);
+
     }
 }
